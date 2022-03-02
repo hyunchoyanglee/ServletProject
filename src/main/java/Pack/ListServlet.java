bac4aee21003da2ad3882f5de8d56ffd45bdce69
Lee jinseon <ter5454@gmail.com>
2022-03-02 15:10:05 +0900

    test

--- src/main/java/Pack/ListServlet.java
+++ src/main/java/Pack/ListServlet.java
@@ -95,7 +95,7 @@ public class ListServlet extends HttpServlet{
 					+ "<a href='index.html'>메인페이지로 이동</a><br/>"
 					+ "</div>"
 					);
-
+			// git test
 		}catch(ClassNotFoundException ce){
 			System.out.println(ce.getMessage());
 		}catch(SQLException se){
