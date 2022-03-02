8fa87b6745b20e632d82f66db4336b063cc82639
Lee jinseon <ter5454@gmail.com>
2022-03-02 14:10:02 +0900

    insert에 region 추가

--- src/main/java/Pack/InsertServlet.java
+++ src/main/java/Pack/InsertServlet.java
@@ -19,21 +19,20 @@ public class InsertServlet extends HttpServlet {
 	@SuppressWarnings("resource")
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html;charset=UTF-8");
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
 		String pwdck = request.getParameter("pwdck");
 		String email = request.getParameter("email");
 		String phone = request.getParameter("phone");
+		String region = request.getParameter("region");
 
 		if(pwd.equals(pwdck)) {
 			int n=0;
 			PreparedStatement pstmt = null;
 			Connection con = null;
-			PrintWriter pw = response.getWriter();
 			try{
 				Class.forName("com.mysql.jdbc.Driver");
-				String url="jdbc:mysql://13.125.251.30:3307/db01?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC";
+				String url = "jdbc:mysql://13.125.251.30:3307/db01?useSSL=false&characterEncoding=UTF-8";
 				con = DriverManager.getConnection(url, "lion", "1234");
 				String name_list = "select count(*) from user where id = ?";
 				pstmt = con.prepareStatement(name_list);
@@ -42,6 +41,8 @@ public class InsertServlet extends HttpServlet {
 				rs.next();
 				if(rs.getInt(1) >= 1) {
 					response.setContentType("text/html;charset=UTF-8");
+					PrintWriter pw = response.getWriter();
+					System.out.println(rs.getRow()+"+"+id);
 					pw.println("<html>");
 					pw.println("<head></head>");
 					pw.println("<body>");
@@ -52,12 +53,13 @@ public class InsertServlet extends HttpServlet {
 					pw.println("</html>");
 					return;
 				}
-				String sql = "insert into user values(null,?,?,?,?,sysdate())";
+				String sql = "insert into user values(null,?,?,?,?,sysdate(),?)";
 				pstmt = con.prepareStatement(sql);
 				pstmt.setString(1, id);
 				pstmt.setString(2, pwd);
 				pstmt.setString(3, email);
 				pstmt.setString(4, phone);
+				pstmt.setInt(5, Integer.parseInt(region));
 				n=pstmt.executeUpdate();
 			}catch(ClassNotFoundException ce){
 				System.out.println(ce.getMessage());
@@ -71,6 +73,8 @@ public class InsertServlet extends HttpServlet {
 					System.out.println(se.getMessage());
 				}
 			}
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter pw = response.getWriter();
 			pw.println("<html>");
 			pw.println("<head></head>");
 			pw.println("<body>");
@@ -87,6 +91,6 @@ public class InsertServlet extends HttpServlet {
 			response.setContentType("text/html;charset=UTF-8");
 			PrintWriter pw = response.getWriter();
 			pw.println ("<html><head><script>alert('비밀번호가 일치하지 않습니다.'); history.go(-1);</script></head></html>");
-		}		
+		}      
 	}
 }
